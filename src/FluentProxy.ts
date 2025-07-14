23d562269b52b2dff1489431b2441e759b1694ba
Marcel <mbos@emico.nl>
2025-07-14 08:45:57 +0200

    Fix: boolean inference issue in Access<T>

--- src/FluentProxy.ts
+++ src/FluentProxy.ts
@@ -2,11 +2,10 @@ export type Primitive = string | number | boolean | null | undefined | symbol;
 
 export type Access<T> = {
   (): T;
-  (value: T, silent?: boolean): void;
-  (updater: (prev: T) => T, silent?: boolean): void;
+  (value: T | ((prev: T) => T)): void;
 };
 
-export type FluentProxyType<T> = T extends Primitive
+export type FluentProxyType<T> = [T] extends [Primitive]
   ? Access<T>
   : T extends Array<infer U>
   ? FluentArrayProxyType<U>
@@ -74,31 +73,33 @@ export class FluentProxy<T> {
     return this._proxy;
   }
 
-  createProxy(path: string[] = []): FluentProxyType<any> {
+  createProxy<T>(path: string[] = []): FluentProxyType<T> {
     const key = path.join(".");
 
     if (this.proxyCache.has(key)) {
-      return this.proxyCache.get(key) as FluentProxyType<any>;
+      return this.proxyCache.get(key) as FluentProxyType<T>;
     }
 
-    const proxyFn = (...args: any[]) => {
+    const proxyFn = ((...args: [] | [T | ((prev: T) => T)]) => {
       if (args.length === 0) {
         if (this.currentExecutionStack !== undefined) {
           this.currentExecutionStack.add([...path].join("."));
         }
-        return getValue(path, this._state);
+        return getValue(path, this._state) as T;
       } else {
         const [arg] = args;
-        const currentValue = getValue(path, this._state);
+        const currentValue = getValue(path, this._state) as T;
         const newValue =
-          typeof arg === "function" ? arg(getValue(path, this._state)) : arg;
+          typeof arg === "function"
+            ? (arg as (prev: T) => T)(currentValue)
+            : arg;
 
         if (!Object.is(currentValue, newValue)) {
           this._state = setValue(path, this._state, newValue);
           this._onChange();
         }
       }
-    };
+    }) as Access<T>;
 
     const proxy = new Proxy(proxyFn, {
       get: (_, prop) => {
@@ -107,7 +108,7 @@ export class FluentProxy<T> {
         }
         return this.createProxy([...path, String(prop)]);
       },
-    }) as FluentProxyType<any>;
+    }) as FluentProxyType<T>;
 
     this.proxyCache.set(key, proxy);
     return proxy;
